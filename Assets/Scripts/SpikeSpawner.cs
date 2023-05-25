8a4a9a848c0d455d621ec4bd45eb643ffeda62ad
Fabio <f.sousasp@hotmail.com>
2023-05-25 17:39:55 -0300

    Implementing difficulty (spawnrate / time)

--- Assets/Scripts/SpikeSpawner.cs
+++ Assets/Scripts/SpikeSpawner.cs
@@ -5,22 +5,43 @@ using UnityEngine;
 public class SpikeSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject spikePrefab;
-    [SerializeField] private float minSpawnDelay = 1.5f;
-    [SerializeField] private float maxSpawnDelay = 3;
+    [SerializeField] private Vector2 easySpawnDelayRange;
+    [SerializeField] private Vector2 hardSpawnDelayRange;
+    [SerializeField] private float timeToMaxDifficulty;
     [SerializeField] private float minH = -1.5f;
     [SerializeField] private float maxH = 1;
+    private float passedTime;
+    private float minSpawnDelay;
+    private float maxSpawnDelay;
+    private float difficulty;
     private float count = 2;
     private float timeToDestroy = 3;
 
     private void Update()
     {
+        UpdateDifficulty();
+        UpdateSpawnTime();
+        countDown();
+    }
+
+    private void UpdateDifficulty(){
+        passedTime += Time.deltaTime;
+        difficulty = passedTime / timeToMaxDifficulty;
+        difficulty = Mathf.Min(1, difficulty);
+    }
+
+    private void UpdateSpawnTime(){
+        minSpawnDelay = Mathf.Lerp(easySpawnDelayRange.x, hardSpawnDelayRange.x, difficulty);
+        maxSpawnDelay = Mathf.Lerp(easySpawnDelayRange.y, hardSpawnDelayRange.y, difficulty);
+    }
+
+    private void countDown(){
         count += Time.deltaTime;
         if(count >= Random.Range(minSpawnDelay, maxSpawnDelay)){
             SpawnSpike();
             count = 0;
         }
     }
-
 
     private void SpawnSpike(){
         Destroy(Instantiate(spikePrefab, transform.position + RandomizeHeight(), Quaternion.identity), timeToDestroy);
