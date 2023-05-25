8152d25c956d21b1bf5351b385d81b9b219a6f6b
Fabio <f.sousasp@hotmail.com>
2023-05-25 18:36:43 -0300

    Add medals at the end of the run

--- Assets/Scripts/UiController.cs
+++ Assets/Scripts/UiController.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UiController : MonoBehaviour
 {
     [SerializeField] private GameObject GameOverUI;
     [SerializeField] private TextMeshProUGUI scoretext;
     [SerializeField] private TextMeshProUGUI bestScoreText;
+    [SerializeField] private Image medalSlot;
+    [SerializeField] private Sprite bronzeMedal;
+    [SerializeField] private Sprite silverMedal;
+    [SerializeField] private Sprite goldMedal;
+    private int score;
 
     private void Start()
     {
@@ -16,16 +22,29 @@ public class UiController : MonoBehaviour
 
     public void GameOver()
     {
+        UpdateMedal();
         UpdateBestScore();
         GameOverUI.SetActive(true);
     }
 
     public void UpdateScore()
     {
-        scoretext.text = Score.Count.ToString();
+        score = Score.Count;
+        scoretext.text = score.ToString();
     }
 
-    private void UpdateBestScore(){
+    private void UpdateBestScore()
+    {
         bestScoreText.text = Score.BestScore.ToString();
+    }
+
+    private void UpdateMedal()
+    {
+        if (score >= Score.BestScore)
+            medalSlot.sprite = goldMedal;
+        else if (score > Score.BestScore / 2)
+            medalSlot.sprite = silverMedal;
+        else
+            medalSlot.sprite = bronzeMedal;
     }
 }
